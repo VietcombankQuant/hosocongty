5de8e3fabd02ffee73b5daef88b09ab366fe5d5a
Mibu287 <buiducminh287@gmail.com>
2023-05-15 19:05:37 +0700

    Add Error type

---
+++ src/errors.rs
@@ -0,0 +1,13 @@
+use libxml::parser::XmlParseError;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("failed to request with error {0:?}")]
+    HttpRequest(#[from] reqwest::Error),
+
+    #[error("failed to parse HTML document with error {0:?}")]
+    HtmlParsing(#[from] XmlParseError),
+
+    #[error("failed to get document nodes from XPATH query {0:?}")]
+    XpathQuerying(String),
+}
