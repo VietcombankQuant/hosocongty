8eecba46a363c235ca9c91ec1e9324b9e5694e0d
Mibu287 <buiducminh287@gmail.com>
2023-05-16 23:10:29 +0700

    Save corporate info to database

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::{ops::Deref, sync::Arc};
+
+use crate::common::Coporate;
 use futures::StreamExt;
 use tokio_stream::wrappers::UnboundedReceiverStream;
 
@@ -7,21 +10,56 @@ mod page;
 mod results;
 mod utils;
 
+fn save_corporate_info(storage: &rocksdb::DB, corporate: &Coporate) -> results::Result<()> {
+    let key = match corporate.get("TaxCode") {
+        Some(key) => key,
+        None => {
+            let err = anyhow::anyhow!(
+                "Failed to get `TaxCode` key from corporate info {:?}",
+                corporate
+            );
+            return Err(results::Error::Other(err));
+        }
+    };
+    let value = serde_json::to_string(&corporate)
+        .map_err(|err| results::Error::Other(anyhow::Error::from(err)))?;
+
+    storage
+        .put(key, &value)
+        .map_err(|err| results::Error::Other(anyhow::Error::from(err)))?;
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<(), results::Error> {
     utils::setup_logger();
 
+    let db_path = "storage";
+
+    let storage = rocksdb::DB::open_default(db_path)
+        .map_err(|err| results::Error::Other(anyhow::Error::from(err)))?;
+    let storage = Arc::new(storage);
+
     let (sender, receiver) = tokio::sync::mpsc::unbounded_channel();
-    tokio::spawn(async move { page::get_links(1..=2, sender).await });
+    tokio::spawn(async move { page::get_links(1..=10, sender).await });
     UnboundedReceiverStream::new(receiver)
         .map(corporate::get_corporate_info)
         .buffer_unordered(8)
-        .map(|result| match result {
+        .map(move |result| (result, storage.clone()))
+        .map(|(result, storage)| match result {
             Err(err) => {
                 log::error!("{}", err);
             }
             Ok(corporate) => {
-                log::info!("{:#?}", corporate);
+                if let Err(err) = save_corporate_info(&storage.deref(), &corporate) {
+                    log::error!("{}", err);
+                }
+
+                log::info!(
+                    "Successfully save corporate info {:?} to database",
+                    corporate
+                );
             }
         })
         .collect::<()>()
